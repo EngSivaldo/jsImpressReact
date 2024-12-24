e490a7df53b4db9c064df3485108e6359389bd22
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2024-12-24 17:31:48 -0300

    A primeira página

--- primeiro-componente/src/App.jsx
+++ primeiro-componente/src/App.jsx
@@ -4,32 +4,9 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const numero = 27
+  const numero2 = 27/3
+ return <h1>Meu primeiro título com o React{numero2}</h1>
 }
 
 export default App
