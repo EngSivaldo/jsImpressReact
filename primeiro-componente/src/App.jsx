1d431ff05a0b7bc52fd8418f35e0e3dd4650553c
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2024-12-24 21:35:23 -0300

    A informação de Estado (STATE)

--- primeiro-componente/src/App.jsx
+++ primeiro-componente/src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const numero = 27
-  const numero2 = 27/3
- return <h1>Meu primeiro título com o React{numero2}</h1>
+  const [numeroAleatorio, alternarNumeroAleatorio] = useState(Math.random());
+  const numero = 27;
+  const numero2 = 27 / 3;
+
+  function trocarNumeroAleatorio() {
+    alternarNumeroAleatorio(Math.random());
+  }
+
+  return (
+    <div>
+      <h1>Meu primeiro título com o React {numeroAleatorio}</h1>
+      <button onClick={trocarNumeroAleatorio}>Novo Número Aleatório</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
