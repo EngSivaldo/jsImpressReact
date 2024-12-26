3872f499506f6294149f919e5e53bdf2b0491c78
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2024-12-26 11:10:18 -0300

    A árvore de componentes e o estado da aplicação

--- MiniProjetoReact/task-list/src/App.jsx
+++ MiniProjetoReact/task-list/src/App.jsx
@@ -1,17 +1,16 @@
+import { useState } from 'react';
 import ItemsContainer from './itemsContainer';
-import './App.css'
+import './App.css';
 
 function App() {
-
+  const [taskList, setTaskList] = useState(['Lavar roupas', 'Ir ao Mercado']);
   return (
-   <div>
-       <h1>Minha lista de tarefas do dia</h1>   
-       <ItemsContainer taskList={['Lavar roupa', 'Ir ao mercado']}/>
-   </div>
-  )
-   
-  
+    <div>
+      <h1>Minha lista de tarefas do dia</h1>
+      <h2>Adição de tarefas</h2>
+      <ItemsContainer taskList={taskList} />
+    </div>
+  );
 }
 
-export default App
-//componente base
+export default App;
