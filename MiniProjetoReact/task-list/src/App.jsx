ff0825d7626c5eb4d67737fdfebf2d74749b54e7
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2024-12-26 21:36:48 -0300

    adicionado comentarios

--- MiniProjetoReact/task-list/src/App.jsx
+++ MiniProjetoReact/task-list/src/App.jsx
@@ -4,26 +4,32 @@ import AddItemsContainer from './AddItemsContainer';
 import './App.css';
 
 function App() {
+  // Função para carregar tarefas do Local Storage
   const loadTasksFromLocalStorage = () => {
     const savedTasks = localStorage.getItem('taskList');
     return savedTasks ? JSON.parse(savedTasks) : [];
   };
 
+  // Estado para armazenar a lista de tarefas
   const [taskList, setTaskList] = useState(loadTasksFromLocalStorage());
 
+  // Função para salvar tarefas no Local Storage
   const saveTasksToLocalStorage = (tasks) => {
     localStorage.setItem('taskList', JSON.stringify(tasks));
   };
 
+  // Efeito para salvar a lista de tarefas no Local Storage sempre que ela mudar
   useEffect(() => {
     saveTasksToLocalStorage(taskList);
   }, [taskList]);
 
+  // Função para remover uma tarefa da lista
   const removeFromList = (item) => {
     const newList = taskList.filter(task => task !== item);
     setTaskList(newList);
   };
 
+  // Função para adicionar uma nova tarefa à lista
   const addToList = (item) => {
     setTaskList([...taskList, item]);
   };
@@ -31,7 +37,9 @@ function App() {
   return (
     <div className="app-container">
       <h1>Minha lista de tarefas do dia</h1>
+      {/* Componente para adicionar novas tarefas */}
       <AddItemsContainer addToList={addToList} />
+      {/* Componente para exibir a lista de tarefas */}
       <ItemsContainer taskList={taskList} removeFromList={removeFromList} />
     </div>
   );
