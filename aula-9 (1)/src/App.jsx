d9a746960ef2fc4b702893e49ef8a8e9551e5772
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2024-12-31 11:08:37 -0300

    Contexto - pt 1 - O Provider

--- aula-9 (1)/src/App.jsx
+++ aula-9 (1)/src/App.jsx
@@ -1,17 +1,26 @@
-import { useState } from "react";
+import { useState, createContext } from "react";
 import "./App.css";
 import FirstWrapper from "./FirstWrapper";
 import TextComponent from "./TextComponent";
 
 function App() {
   const [color, setColor] = useState("blue");
+  const colorContext = createContext(null);
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-      <TextComponent color={color}>Daniel do JavaScript Impressionador</TextComponent>
-      <FirstWrapper setColor={setColor} />
-    </div>
-  );
+    <colorContext.Provider value={{
+      color,
+      setColor,
+    }}>
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <TextComponent color={color}>Daniel do JavaScript Impressionador</TextComponent>
+        <FirstWrapper setColor={setColor} />
+      </div>
+    </colorContext.Provider>
+    );
 }
+  
+    
+   
 
 export default App;
